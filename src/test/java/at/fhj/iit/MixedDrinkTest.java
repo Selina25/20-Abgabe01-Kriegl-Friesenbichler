40d6cf13d242db80bfad9b22d2d24bf3e0f03adc
MarleneKr <marlene.kriegl@gmail.com>
2020-05-09 08:58:25 +0200

    created MixedDrinkTest class and associated java docs

---
+++ src/test/java/at/fhj/iit/MixedDrinkTest.java
@@ -0,0 +1,74 @@
+package test.java.at.fhj.iit;
+
+
+import main.java.at.fhj.iit.MixedDrink;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MixedDrinkTest{
+
+    /**
+     * BeforeEach: init multiple mixed drinks (one for each construtor)
+     */
+
+    /**
+     * test the first constuctor (with name)
+     */
+
+    /**
+     * test the second constructor (name, two liquids)
+     */
+
+    /**
+     * test the third constructor (name, two liquids, one syrup)
+     */
+
+    /**
+     * test the fourth constructor (name, two liquids and a list with other ingredients)
+     */
+
+    /**
+     * test the fifth constructor (name, two liquids, one syrup and a list with other ingredients)
+     */
+
+    /**
+     * test the volume getter method
+     */
+
+    /**
+     * test the alcoholPercent getter method
+     */
+
+    /**
+     * test the isAlcoholic() method
+     */
+
+    /**
+     * test the shake() method
+     */
+
+    /**
+     * test the stir() method
+     */
+
+    /**
+     * test the otherIngredients getter method
+     */
+
+    /**
+     * test the otherIngredients setter method
+     */
+
+    /**
+     * test the addOtherIngredients() method
+     */
+
+    /**
+     * test the syrup getter method
+     */
+
+    /**
+     * test the syrup setter method
+     */
+}
