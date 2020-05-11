b7664231dfb7a19a91ae736d8181e4ee14a7b079
Selina Friesenbichler <s.friesenbichler@edu.campus02.at>
2020-05-11 16:45:56 +0200

    Added unimplemented SimpleDrinkTest (including Javadocs)

---
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -0,0 +1,38 @@
+package test.java.at.fhj.iit;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import main.java.at.fhj.*   ;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+@DisplayName("Testing SimpleDrink implementation")
+public class SimpleDrinkTest {
+
+
+    /**
+     * inits an alcoholic liquid and an non-alcoholic Liquid to test the constructor of SimpleDrink
+     */
+    @BeforeEach
+
+
+    /**
+     * tests the constructor of the class SimpleDrink with an non-alcoholic Liquid
+     */
+    @Test
+    @DisplayName("Testing constructor non alcoholic")
+
+
+    /**
+     * tests the constructor of the class SimpleDrink with an alcoholic Liquid
+     */
+    @Test
+    @DisplayName("Testing constructor alcoholic")
+
+
+
+
+}
