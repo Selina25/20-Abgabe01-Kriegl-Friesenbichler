21716161b36f226b8cb11e3fc8ea47c7eb8cec70
MarleneKr <marlene.kriegl@gmail.com>
2020-05-12 16:06:41 +0200

    formatted MixedDrink

--- src/main/java/at/fhj/iit/MixedDrink.java
+++ src/main/java/at/fhj/iit/MixedDrink.java
@@ -4,7 +4,6 @@ package at.fhj.iit;
 import java.util.List;
 
 
-
 /**
  * Class represents a mixed drink which can has to contain one alcoholic and one non-alcoholic liquid
  * and can be refined with different other ingredients and one syrup.
@@ -16,7 +15,7 @@ public class MixedDrink extends Drink {
      * This enum contains all possible syrup which can be added to the mixed drink
      */
     protected enum Syrup {
-       elderberry,
+        elderberry,
         blueberry,
         strawberry,
         peach,
@@ -26,19 +25,19 @@ public class MixedDrink extends Drink {
     }
 
     /**
-     *  name of liquid number one
+     * name of liquid number one
      */
-    protected  Liquid l1;
+    protected Liquid l1;
     /**
-     *  name of liquid number two
+     * name of liquid number two
      */
-    protected  Liquid l2;
+    protected Liquid l2;
     /**
-     *  name of the list with the extra igrendients
+     * name of the list with the extra igrendients
      */
     protected List<String> otherIngredients;
     /**
-     *  name of the syrup which can be added
+     * name of the syrup which can be added
      */
     protected Syrup syrup;
 
@@ -47,46 +46,44 @@ public class MixedDrink extends Drink {
      * Creates a MixedDrink object with given name
      *
      * @param name name of the drink
-     *
      */
-    public MixedDrink(String name){
+    public MixedDrink(String name) {
         super(name);
 
     }
+
     /**
      * Creates a MixedDrink object with given name and two Liquids
      *
      * @param name name of the drink
-     * @param l1 name of the first liquid
-     * @param l2 name of the second liquid
-     *
+     * @param l1   name of the first liquid
+     * @param l2   name of the second liquid
      */
     MixedDrink(String name, Liquid l1, Liquid l2) throws AlcoholicSubstanceException {
         super(name);
-        if(liquidsAlcoholic(l1, l2) == false) {
+        if (liquidsAlcoholic(l1, l2) == false) {
             this.l1 = l1;
             this.l2 = l2;
-        }else{
+        } else {
             throw new AlcoholicSubstanceException();
         }
     }
 
 
-
     /**
      * Creates a MixedDrink object with given name and two Liquids
      *
-     * @param name name of the drink
-     * @param l1 name of the first liquid
-     * @param l2 name of the second liquid
+     * @param name  name of the drink
+     * @param l1    name of the first liquid
+     * @param l2    name of the second liquid
      * @param syrup name of the sirup
      */
     MixedDrink(String name, Liquid l1, Liquid l2, Syrup syrup) throws AlcoholicSubstanceException {
         super(name);
-        if(liquidsAlcoholic(l1, l2) == false) {
+        if (liquidsAlcoholic(l1, l2) == false) {
             this.l1 = l1;
             this.l2 = l2;
-        }else{
+        } else {
             throw new AlcoholicSubstanceException();
         }
         this.syrup = syrup;
@@ -96,17 +93,17 @@ public class MixedDrink extends Drink {
     /**
      * Creates a MixedDrink object with given name and two Liquids
      *
-     * @param name name of the drink
-     * @param l1 name of the first liquid
-     * @param l2 name of the second liquid
-     * @param otherIngredients  list of names of other ingredients
+     * @param name             name of the drink
+     * @param l1               name of the first liquid
+     * @param l2               name of the second liquid
+     * @param otherIngredients list of names of other ingredients
      */
-    MixedDrink(String name, Liquid l1, Liquid l2, List<String>otherIngredients) throws AlcoholicSubstanceException {
+    MixedDrink(String name, Liquid l1, Liquid l2, List<String> otherIngredients) throws AlcoholicSubstanceException {
         super(name);
-        if(liquidsAlcoholic(l1, l2) == false) {
+        if (liquidsAlcoholic(l1, l2) == false) {
             this.l1 = l1;
             this.l2 = l2;
-        } else{
+        } else {
             throw new AlcoholicSubstanceException();
         }
         this.otherIngredients = otherIngredients;
@@ -116,18 +113,18 @@ public class MixedDrink extends Drink {
     /**
      * Creates a MixedDrink object with given name and two Liquids
      *
-     * @param name name of the drink
-     * @param l1 name of the first liquid
-     * @param l2 name of the second liquid
-     * @param syrup name of the sirup
-     * @param otherIngredients  list of names of other ingredients
+     * @param name             name of the drink
+     * @param l1               name of the first liquid
+     * @param l2               name of the second liquid
+     * @param syrup            name of the sirup
+     * @param otherIngredients list of names of other ingredients
      */
-    MixedDrink(String name, Liquid l1, Liquid l2, Syrup syrup, List<String>otherIngredients) throws AlcoholicSubstanceException {
+    MixedDrink(String name, Liquid l1, Liquid l2, Syrup syrup, List<String> otherIngredients) throws AlcoholicSubstanceException {
         super(name);
-            if(liquidsAlcoholic(l1, l2) == false) {
+        if (liquidsAlcoholic(l1, l2) == false) {
             this.l1 = l1;
             this.l2 = l2;
-        }else{
+        } else {
             throw new AlcoholicSubstanceException();
         }
         this.otherIngredients = otherIngredients;
@@ -136,9 +133,10 @@ public class MixedDrink extends Drink {
 
     /**
      * getter for name
+     *
      * @return name from super class 'Drink'
      */
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
@@ -149,7 +147,7 @@ public class MixedDrink extends Drink {
      */
     @Override
     public double getVolume() {
-        return l1.getVolume()+l2.getVolume();
+        return l1.getVolume() + l2.getVolume();
     }
 
     /**
@@ -159,7 +157,7 @@ public class MixedDrink extends Drink {
      */
     @Override
     public double getAlcoholPercent() {
-        return (l1.getAlcoholPercent()+l2.getAlcoholPercent())/2;
+        return (l1.getAlcoholPercent() + l2.getAlcoholPercent()) / 2;
     }
 
     /**
@@ -169,9 +167,9 @@ public class MixedDrink extends Drink {
      */
     @Override
     public boolean isAlcoholic() {
-        if(l1.getAlcoholPercent() > 0 || l2.getAlcoholPercent() > 0){
+        if (l1.getAlcoholPercent() > 0 || l2.getAlcoholPercent() > 0) {
             return true;
-        }else {
+        } else {
             return false;
         }
 
@@ -179,14 +177,15 @@ public class MixedDrink extends Drink {
 
     /**
      * This method checks if both liquids are alcoholic
+     *
      * @param l1 Liquid 1
      * @param l2 Liquid 2
      * @return true, if both liquids are alcoholic
      */
     public boolean liquidsAlcoholic(Liquid l1, Liquid l2) {
-        if(l1.getAlcoholPercent() > 0 && l2.getAlcoholPercent() > 0){
+        if (l1.getAlcoholPercent() > 0 && l2.getAlcoholPercent() > 0) {
             return true;
-        }else {
+        } else {
             return false;
         }
 
@@ -195,20 +194,21 @@ public class MixedDrink extends Drink {
     /**
      * Shakes the ingredients of the mixed drink
      */
-    public void shake(){
-    System.out.println("The ingredients have been shook and the drink is now ready to serve");
+    public void shake() {
+        System.out.println("The ingredients have been shook and the drink is now ready to serve");
 
     }
 
     /**
      * Stirs the ingredients of the mixed drink
      */
-    public  void  stir(){
+    public void stir() {
         System.out.println("The ingredients have been stirred and the drink is now ready to serve");
     }
 
     /**
      * Getter for the List of extra ingredients
+     *
      * @@return otherIngredients list of all extra ingredients
      */
     public List<String> getOtherIngredients() {
@@ -218,7 +218,7 @@ public class MixedDrink extends Drink {
     /**
      * Setter for the List of the extra ingredients
      *
-     * @param otherIngredients  list of all extra ingredients
+     * @param otherIngredients list of all extra ingredients
      */
     public void setOtherIngredients(List<String> otherIngredients) {
         this.otherIngredients = otherIngredients;
@@ -250,8 +250,6 @@ public class MixedDrink extends Drink {
     public void setSyrup(Syrup syrup) {
         this.syrup = syrup;
     }
-
-
 
 
 }
